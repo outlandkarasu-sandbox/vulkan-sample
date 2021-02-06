bcd0eabc802ad48dde70c22045aaa460eff2a92f
outland.karasu <outland.karasu@gmail.com>
2021-02-06 22:22:55 +0900

    create and destroy device.

--- source/app.d
+++ source/app.d
@@ -1,4 +1,4 @@
-import std.algorithm : map, each;
+import std.algorithm : map, each, filter;
 import std.string : toStringz;
 import std.stdio : writeln, writefln;
 import std.exception : enforce;
@@ -8,21 +8,31 @@ import erupted :
     loadInstanceLevelFunctions,
     VK_MAKE_VERSION,
     VK_NULL_HANDLE,
+    VK_QUEUE_GRAPHICS_BIT,
     VkApplicationInfo,
+    vkCreateDevice,
     vkCreateInstance,
     vkDestroyInstance,
+    VkDevice,
+    VkDeviceCreateInfo,
+    VkDeviceQueueCreateInfo,
     vkEnumeratePhysicalDevices,
     vkGetPhysicalDeviceFeatures,
     vkGetPhysicalDeviceProperties,
+    vkGetPhysicalDeviceQueueFamilyProperties,
     VkInstance,
     VkInstanceCreateInfo,
     VkPhysicalDevice,
     VkPhysicalDeviceFeatures,
     VkPhysicalDeviceProperties,
+    VkQueueFamilyProperties,
     VkResult;
 
 import erupted.vulkan_lib_loader :
     loadGlobalLevelFunctions;
+
+import erupted.dispatch_device :
+    DispatchDevice;
 
 import karasutk.sdl :
     duringSDL,
@@ -82,7 +92,8 @@ void vulkanMain()
 
     enforce(physDevices.length > 0, "Device not found.");
 
-    foreach (device; physDevices) {
+    foreach (device; physDevices)
+    {
         VkPhysicalDeviceProperties properties;
         device.vkGetPhysicalDeviceProperties(&properties);
         properties.writeln;
@@ -92,6 +103,46 @@ void vulkanMain()
         features.writeln;
     }
 
+    auto physDevice = physDevices[0];
+
+    uint queueFamilyCount;
+    physDevice.vkGetPhysicalDeviceQueueFamilyProperties(&queueFamilyCount, null);
+    auto queueFamilies = new VkQueueFamilyProperties[](queueFamilyCount);
+    physDevice.vkGetPhysicalDeviceQueueFamilyProperties(&queueFamilyCount, queueFamilies.ptr);
+
+    uint queueIndex = 0;
+    foreach (queue; queueFamilies)
+    {
+        if (queue.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            break;
+        }
+        ++queueIndex;
+    }
+
+    enforce(queueIndex < queueFamilies.length, "Queue family not found.");
+
+    float queuePriority = 1.0f;
+    VkDeviceQueueCreateInfo deviceQueueCreateInfo = {
+        queueFamilyIndex: queueIndex,
+        queueCount: 1,
+        pQueuePriorities: &queuePriority,
+    };
+
+    VkPhysicalDeviceFeatures physDeviceFeatures;
+    VkDeviceCreateInfo deviceCreateInfo = {
+        pQueueCreateInfos: &deviceQueueCreateInfo,
+        queueCreateInfoCount: 1,
+        pEnabledFeatures: &physDeviceFeatures,
+        enabledExtensionCount: 0,
+        enabledLayerCount: 0,
+    };
+
+    VkDevice device;
+    enforceVK(physDevice.vkCreateDevice(&deviceCreateInfo, null, &device));
+    auto dispatchDevice = DispatchDevice(device);
+
+    scope(exit) dispatchDevice.DestroyDevice();
 
     EventHandlers eventHandlers;
     window.runEventLoop(eventHandlers);
